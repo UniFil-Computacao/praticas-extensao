0bcefdf231d8a8b82c0c2b0798af373609be261b
joao951951 <joao159@edu.unifil.br>
2022-11-12 23:54:55 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'fast-and-flash',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
